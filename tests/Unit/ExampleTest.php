62c9fadb4792a208f5190d0a0706aa68c4c5b15a
Rogerio Pereira <hi@rogeriopereira.dev>
2022-12-06 21:11:54 -0500

    AULA: 04 - INSTALAR O PACKAGE MOCKERY

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,12 +2,18 @@
 
 namespace Tests\Unit;
 
+use Mockery;
+use stdClass;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
     public function test_example()
     {
+        Mockery::mock(stdClass::class)
+            ->shouldReceive('test')
+            ->andReturn(true);
+            
         $this->assertTrue(true);
     }
 }
