9560a6eaba6da5ae16e8ba6ac2eae44145172041
Rogerio Pereira <hi@rogeriopereira.dev>
2022-12-08 16:39:31 -0500

    AULA: 03 - TRABALHAR COM UUID V4 NO LARAVEL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\UserObserver;
+use Illuminate\Support\ServiceProvider;
 use App\Repository\Eloquent\UserRepository;
 use App\Repository\Interfaces\UserRepositoryInterface;
-use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
     }
 }
